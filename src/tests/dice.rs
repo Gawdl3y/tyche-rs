a963843afa634afe2cf3bf39ec5720f496e5baea
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2024-02-23 13:54:00 -0500

    Add tests for dice and roll equality/inequality

--- src/tests/dice.rs
+++ src/tests/dice.rs
@@ -64,12 +64,81 @@ fn all_dice_sides_occur() {
 	}
 }
 
+#[test]
+fn dice_equality() {
+	let da = Dice::new(4, 8);
+	let db = Dice::new(4, 8);
+	assert_eq!(da, db);
+}
+
+#[test]
+fn dice_inequality() {
+	let da = Dice::new(4, 8);
+	let db = Dice::new(4, 20);
+	assert_ne!(da, db);
+
+	let da = Dice::new(4, 8);
+	let db = Dice::new(2, 8);
+	assert_ne!(da, db);
+
+	let da = Dice::new(4, 8);
+	let db = Dice::new_exploding(4, 8);
+	assert_ne!(da, db);
+}
+
+#[test]
+fn roll_equality() {
+	let da = Dice::new_exploding(4, 8);
+	let db = Dice::new_exploding(4, 8);
+	let ra = Roll {
+		rolls: vec![4, 5, 8, 8],
+		explosions: Some(vec![4, 7]),
+		dice: &da,
+	};
+	let rb = Roll {
+		rolls: vec![4, 5, 8, 8],
+		explosions: Some(vec![4, 7]),
+		dice: &db,
+	};
+	assert_eq!(ra, rb);
+}
+
+#[test]
+fn roll_inequality() {
+	let da = Dice::new_exploding(4, 8);
+	let db = Dice::new_exploding(4, 8);
+	let ra = Roll {
+		rolls: vec![4, 5, 8, 8],
+		explosions: Some(vec![4, 7]),
+		dice: &da,
+	};
+	let rb = Roll {
+		rolls: vec![4, 6, 8, 8],
+		explosions: Some(vec![4, 7]),
+		dice: &db,
+	};
+	assert_ne!(ra, rb);
+
+	let da = Dice::new_exploding(4, 8);
+	let db = Dice::new(4, 8);
+	let ra = Roll {
+		rolls: vec![4, 5, 8, 8],
+		explosions: Some(vec![4, 7]),
+		dice: &da,
+	};
+	let rb = Roll {
+		rolls: vec![4, 5, 8, 8],
+		explosions: None,
+		dice: &db,
+	};
+	assert_ne!(ra, rb);
+}
+
 fn construct_plain(count: u8, sides: u8) -> Dice {
 	let dice = Dice::new(count, sides);
 	assert_eq!(dice.count, count);
 	assert_eq!(dice.sides, NonZeroU8::new(sides).unwrap());
 	assert!(!dice.explode);
-
 	dice
 }
 
@@ -78,7 +147,6 @@ fn construct_exploding(count: u8, sides: u8) -> Dice {
 	assert_eq!(dice.count, count);
 	assert_eq!(dice.sides, NonZeroU8::new(sides).unwrap());
 	assert!(dice.explode);
-
 	dice
 }
 
