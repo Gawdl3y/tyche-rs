61e83e8a05bb4e8b7a0da244009ecb5a645894ba
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2024-03-30 19:57:00 -0400

    Add parser benches for FromStr implementation

--- benches/parse.rs
+++ benches/parse.rs
@@ -3,7 +3,10 @@
 extern crate test;
 
 use chumsky::Parser;
+use dicey::expr::Expr;
 use test::Bencher;
+
+const ABSURD_EXPR: &str = include_str!("absurd_dice_expr.txt");
 
 #[bench]
 fn parse_basic(b: &mut Bencher) {
@@ -20,10 +23,25 @@ fn parse_complex(b: &mut Bencher) {
 #[bench]
 fn parse_absurd(b: &mut Bencher) {
 	let parser = dicey::parser();
-	b.iter(|| parser.parse(include_str!("absurd_dice_expr.txt")))
+	b.iter(|| parser.parse(ABSURD_EXPR).unwrap())
 }
 
 #[bench]
 fn parser_creation(b: &mut Bencher) {
 	b.iter(|| dicey::parser());
+}
+
+#[bench]
+fn fromstr_basic(b: &mut Bencher) {
+	b.iter(|| "4d8 + 4".parse::<Expr>().unwrap());
+}
+
+#[bench]
+fn fromstr_complex(b: &mut Bencher) {
+	b.iter(|| "4d8x + 2d10 * (-3d6 - 6 / 2 \\ 4)".parse::<Expr>().unwrap());
+}
+
+#[bench]
+fn fromstr_absurd(b: &mut Bencher) {
+	b.iter(|| ABSURD_EXPR.parse::<Expr>().unwrap())
 }
